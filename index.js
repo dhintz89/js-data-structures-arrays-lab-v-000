5e489d6742058e4be5522f78c01730f758c48bb1
Dan Hintz <dhintz@live.com>
2019-06-22 01:25:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ function destructivelyAppendDriver(name) {
 }
 
 function destructivelyPrependDriver(name) {
-  drivers.unshift(name)
+  drivers.unshift(name);
 }
 
 function destructivelyRemoveLastDriver() {
-  drivers.pop()
+  drivers.pop();
 }
 
 function destructivelyRemoveFirstDriver() {
-  drivers.shift()
+  drivers.shift();
 }
 
 function appendDriver(name) {
-  const newArray = drivers.
+  const newArray = [...drivers, name];
 }
 
 function prependDriver(name) {
-  
+  const newArray = [name, ...drivers];
 }
 
 function removeLastDriver() {
-  const newArray = drivers.slice(-1)
+  const newArray = drivers.slice(-1);
 }
 
 function removeFirstDriver() {
-  const newArray = drivers.slice(0,1)
+  const newArray = drivers.slice(0,1);
 }
